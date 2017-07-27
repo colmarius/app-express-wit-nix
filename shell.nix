984acce3101cade426d9142203545efd07c99389
Marius Colacioiu <marius.colacioiu@gmail.com>
2017-07-27 11:42:08 +0200

    Build node_modules as nix package

--- shell.nix
+++ shell.nix
@@ -1,11 +1,27 @@
 with import (fetchTarball {
   url = "https://github.com/nixos/nixpkgs/archive/00512470ec241949148b61e1c120fa76d685cf9a.tar.gz";
 }) {};
+let
+  nodejs = nodejs-8_x;
+  yarn2nix = (import (fetchTarball {
+    url = "https://github.com/zimbatm/yarn2nix/archive/e3534d6071b9b6517bbc4e3fdfccf377c96a3c12.tar.gz";
+  }) { inherit pkgs nodejs; });
+  yarnModules = yarn2nix.mkYarnModules {
+    name = "myapp-yarn-modules";
+    packageJSON = ./package.json;
+    yarnLock = ./yarn.lock;
+  };
+in
 stdenv.mkDerivation {
   name = "myapp";
   buildInputs = [
     bashInteractive
     nodejs-8_x
     yarn
+    nixUnstable
   ];
+  shellHook = ''
+    rm -rf node_modules
+    ln -s ${yarnModules}/node_modules node_modules
+  '';
 }
